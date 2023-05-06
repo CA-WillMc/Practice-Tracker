60a598934bf7035c30be0d625ae127fd8c24baaf
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-05-06 15:27:36 -0400

    addGoals to toDoList

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useState, useRef } from "react";
 import "./menu.css";
+import "./index.css";
 
 export default function Menu() {
   function handleLinkClick(event) {
@@ -7,6 +9,31 @@ export default function Menu() {
     const targetId = event.currentTarget.getAttribute("href").slice(1);
     const targetElement = document.getElementById(targetId);
     targetElement.scrollIntoView({ behavior: "smooth" });
+  }
+  const [todos, setTodos] = useState([]);
+  const textInputRef = useRef();
+  const numberInputRef = useRef();
+  /*
+  function handleInputChange(event) {
+    const newTodo = setNewTodo(event.target.value);
+    setNewTodo([...todos, newTodo]);
+  }
+*/
+  function handleAddTodoClick() {
+    const newTodo =
+      "Practice " +
+      textInputRef.current.value +
+      " for " +
+      numberInputRef.current.value +
+      "!";
+    setTodos([...todos, newTodo]);
+    textInputRef.current.value = "";
+    numberInputRef.current.value = "";
+  }
+  function handleDeleteTodoClick(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
   }
   return (
     <>
@@ -36,12 +63,29 @@ export default function Menu() {
       <body>
         <section id="Goals">
           <h2>Goals</h2>
-          <input type="text" placeholder="goals"></input>
-          <button>add</button>
+          <div id="addTodo">
+            <input type="text" placeholder="Goal" ref={textInputRef}></input>
+            <input
+              type="text"
+              placeholder="repetition"
+              ref={numberInputRef}
+            ></input>
+            <button onClick={handleAddTodoClick}>add</button>
+          </div>
           <h3>To do list</h3>
-          <ul id="Todo">
-            <li>scales</li>
-            <li>arpegios</li>
+          <ul id="listTodo">
+            {todos.map((todo, index) => (
+              <li className="elemTodo" key={index}>
+                {todo}
+                <button id="practiceTodo">:)</button>
+                <button
+                  id="deleteTodo"
+                  onClick={() => handleDeleteTodoClick(index)}
+                >
+                  X
+                </button>
+              </li>
+            ))}
           </ul>
         </section>
         <section id="Calendar">
